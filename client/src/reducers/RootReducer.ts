1ce9da2537dd024aec8233046980c36d693f61ce
Victor Guiliana <adumith_91@hotmail.com>
2020-12-09 19:04:25 -0300

    firts commit typescript project

---
+++ client/src/reducers/RootReducer.ts
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+import pokemonReducer from "./PokemonReducer";
+
+//                                     reducers: {}
+const RootReducer = combineReducers({
+    pokemon: pokemonReducer
+});
+
+export default RootReducer;
